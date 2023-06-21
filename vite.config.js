2d32f5cbffd2d602512a1d93b9383d0233372ed2
YeenYan <markiantalan@gmail.com>
2023-06-21 19:00:19 +0800

    fix bug 4

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/takenote-app',
+  base: '/takenote-app/',
   // base: '/localhost:5173/takenote-app',
   server: {
     port: 5173, // Set the desired port number
