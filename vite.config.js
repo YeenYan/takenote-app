86c1443e1faef1d7edb6c8b4c769fed79a936f6a
YeenYan <markiantalan@gmail.com>
2023-06-21 18:03:01 +0800

    fix conflicts

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,14 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // },
+  base: '/takenote-app',
+  // base: '/localhost:5173/takenote-app',
+  server: {
+    port: 5173, // Set the desired port number
   },
-  base: '/takenote-app/',
 });
