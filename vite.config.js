f0208a4f764431fbecbb8b8dd1f9ae5f2f6d26f6
YeenYan <markiantalan@gmail.com>
2023-06-27 09:50:18 +0800

    configure some changes

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,24 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/takenote-app/',
+  base: '/dist/',
   plugins: [vue()],
   // resolve: {
   //   alias: {
   //     '@': fileURLToPath(new URL('./src', import.meta.url))
   //   }
   // },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 });
